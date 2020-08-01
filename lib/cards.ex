8b75408522c84125a4297d5c27a27265528f95dc
xpy <cpiwarski01@gmail.com>
2020-07-31 19:21:42 -0700

    lib: Create cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -12,7 +12,21 @@ defmodule Cards do
       :world
 
   """
-  def hello do
-    :world
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
   end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
 end
